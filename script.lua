08011a00ebb2b613139a23b1227dfe2b853ac52e
Yahahahau <arom.phum7154@gmail.com>
2025-06-16 20:13:50 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,15 @@
+local player = game.Players.LocalPlayer
+
 while true do
-    for _, tool in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-        tool:Activate()
+    for _, tool in pairs(player.Backpack:GetChildren()) do
+        if tool:IsA("Tool") then
+            
+            player.Character.Humanoid:EquipTool(tool)
+            
+            wait(0.1)
+            
+            tool:Activate()
+        end
     end
     wait(0.01)
 end
